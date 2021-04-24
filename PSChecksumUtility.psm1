800b61d59b9abcbf415692ed05af9fdd67c6b1c6
0xDAWS <81747134+0xDAWS@users.noreply.github.com>
2021-04-24 19:14:08 +0930

    Update PSChecksumUtility.psm1
    - Changed Check-FileExists to Get-FileExists
    - Cleaned up formatting

--- PSChecksumUtility.psm1
+++ PSChecksumUtility.psm1
@@ -6,37 +6,27 @@
 <#
  .Synopsis
   Generates checksums for files and directories
-
  .Description
   Generates checksum file hashes for a variety of algorithms for the purposes of file validation.
-
  .Parameter Mode
   Sets the mode of operation.
-
  .Parameter Path
   The path to a given file or directory.
-
  .Parameter Algorithm
   Sets the hashing algorithm to be used in the checksum operation (Default: SHA256).
-
  .Parameter Hash
   Specific previously known hash, which can be used in Compare mode.
-
  .Parameter OutFile
   Boolean value that when set will generate a checksum file in the same directory as -Path.
-
  .Example
    # Checksum a single file, and output to console.
    PSCheckSum -Mode File -Path C:\Test\TestDoc-1.txt
-
  .Example
    # Checksum an entire directory using the MD5 algorithm and create a checksum file.
    PSCheckSum -Mode Directory -Path C:\Test\ -Algorithm MD5 -OutFile
-
  .Example
    # Perform a check operation on a checksum file.
    PSCheckSum -Mode Check -Path C:\Test\MD5SUMS.txt -Algorithm MD5
-
  .Example
    # Perform a compare operation on a file with a known hash
    PSCheckSum -Mode Compare -Path C:\Test\TestDoc-1.txt -Algorithm MD5 -Hash 4765C0B343E5431B301BC375628B4DED
@@ -58,7 +48,7 @@ function PSCheckSum {
         return $FileResults.Hash + "  " + $FileResults.Path
     }
 
-    function Check-FileExists([string]$fn) {
+    function Get-FileExists([string]$fn) {
         return Test-Path -Path $fn -PathType Leaf
     }
 
@@ -82,10 +72,9 @@ function PSCheckSum {
             $SplitChecksum = $Checksum.Split("", 2)
             $StoredChecksum = $SplitChecksum[0].Trim().ToUpper()
             $PathToFile = $SplitChecksum[1].Trim()
-            $PathVaild = Check-FileExists($PathToFile)
 
             # If the file can't be found then try searching in the same directory as the checksum file
-            if ( -not $PathVaild ) {
+            if ( -not (Get-FileExists($PathToFile)) ) {
                 $CheckSumFileDirectoryPath = (Get-Item $Path).Directory.FullName
                 $PathToFile = [System.IO.Path]::Combine($CheckSumFileDirectoryPath, $PathToFile)
             }
@@ -114,25 +103,20 @@ function PSCheckSum {
         Write-Verbose "Hash: $Hash"
         if ( $Hash -and $Path ) { 
 
-            if (Check-FileExists($Path)) { 
+            if (Get-FileExists($Path)) { 
                 $FileHash = (Get-FileHash -Algorithm $Algorithm $Path).Hash
-
                 if ($Hash.Equals($FileHash)) { 
                     Write-Host -ForegroundColor Green "[ PASS ]" $Path
                 }
-
                 else {
                     Write-Host -ForegroundColor Red "[ FAIL ]" $Path
                 }
-
                 Write-Host -ForegroundColor Green "[+] Operation Complete!" 
             }
-
             else {
                 Write-Host -ForegroundColor Red "[-] File does not exist at this path:" $Path
             }
         }
-
         else {
             Write-Host -ForegroundColor Red "[-] To use Compare mode, both the -Hash and -Path flags must be set correctly"
         }
@@ -150,7 +134,7 @@ function PSCheckSum {
             # If SHA256SUMS.txt already exists, remove it
             if ($OutFile) {
                 $OutFilePath = [IO.Path]::Combine($Path, $OutFileName)
-                if ( Check-FileExists($OutFilePath) ) { 
+                if ( Get-FileExists($OutFilePath) ) { 
                     Remove-Item $OutFilePath
                 }
             }
@@ -158,10 +142,8 @@ function PSCheckSum {
             $Files = @(Get-ChildItem $Path)
             foreach ($f in $Files) {
                 $FilePath = [IO.Path]::Combine($Path,$f)
-                if (Check-FileExists($FilePath)) {
+                if (Get-FileExists($FilePath)) {
                     $FileChecksum = Get-FileChecksum($FilePath)
-
-                    # Check that OutFile is set or not set, and display/write checksum content
                     if ($OutFile) {
                         Add-Content $OutFilePath $FileChecksum
                         $FileChecksum
@@ -190,26 +172,20 @@ function PSCheckSum {
         Write-Verbose "Algorithm: $Algorithm"
         Write-Verbose "Generate checksums file: $OutFile"
 
-        if ( Check-FileExists($Path) ) {
-
+        if ( Get-FileExists($Path) ) {
             $FileChecksum = Get-FileChecksum($Path)
-
             if ($OutFile) {
                 $DirPath = (Get-Item $Path).Directory.FullName
                 $OutFilePath = [IO.Path]::Combine($DirPath, $OutFileName)
-
-                if ( Check-FileExists($OutFilePath) ) { 
+                if ( Get-FileExists($OutFilePath) ) { 
                     Remove-Item $OutFilePath
                 }
-            
                 Add-Content $OutFilePath $FileChecksum
                 $FileChecksum
             }
-
             else {
                 $FileChecksum
             }
-
             Write-Host -ForegroundColor Green "[+] Operation Complete!" 
         }
         else {
